b5e5e246efcba25dc8258e69fb0f7b4c8f010458
Kasturi Chaudhari <kasturi.chaudhari11@gmail.com>
2022-08-10 15:04:19 -0700

    completed Objective2Lab2.java

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Kasturi Chaudhari";
+    int age = 34;
+    String state = "Maharashtra";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
