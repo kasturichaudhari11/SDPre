0943312902896140c94261c051d8bb1a5cbac6ca
Kasturi Chaudhari <kasturi.chaudhari11@gmail.com>
2022-08-10 19:56:57 -0700

    completed Objective3Lab1.java

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,13 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+    num1 = 1.2;
+    num2 = 3.5;
+    num3 = 5.6;
+
+    average = (num1 + num2 + num3)/3;
+
+    System.out.println(average);
+
+  }
+}
