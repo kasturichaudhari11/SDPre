191e757235c2b74ba412eebda678bedcf7269196
Kasturi Chaudhari <kasturi.chaudhari11@gmail.com>
2022-08-10 22:35:47 -0700

    completed Objective3Lab2.java

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,12 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1, side2, hypotenus;
+
+    side1 = 10.0;
+    side2 = 8.0;
+    hypotenus = Math.sqrt((side1*side1) + (side2*side2));
+
+    System.out.println(hypotenus);
+
+  }
+}
