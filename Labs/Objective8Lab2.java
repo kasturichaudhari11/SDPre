18330b2d3ccb411a7aa2d6e60c5de6e72c1a4b7f
Kasturi Chaudhari <kasturi.chaudhari11@gmail.com>
2022-08-12 13:41:12 -0700

    completed Objective8Lab2.java

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int sum = 0;
+
+    for(int num = 1; num <= 20; num++) {
+      sum += num;
+    }
+    System.out.println(sum);
+  }
+}
